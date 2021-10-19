02787867c25d7db50af46aeed53571535f1c97a9
Vera Yeh <brisa8281@gmail.com>
2021-10-18 19:45:43 -0700

    Completed react-form-controls.

---
+++ react-form-controls/src/index.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class NewsletterForm extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = { email: '' };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({ email: event.target.value });
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    console.log('component state: ', this.state);
+  }
+
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <label>Email
+            <input onChange={this.handleChange} type="email" />
+          </label>
+          <input type="submit" value="Sign Up" />
+        </form>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<NewsletterForm />, document.getElementById('root'));
