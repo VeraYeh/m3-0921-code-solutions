e8b70a3f4c1c74904de92ac7e6e914c0ae3df55c
Vera Yeh <brisa8281@gmail.com>
2021-10-18 19:58:18 -0700

    Updated input attribute on line 27.

--- react-form-controls/src/index.jsx
+++ react-form-controls/src/index.jsx
@@ -24,7 +24,7 @@ class NewsletterForm extends React.Component {
       <div>
         <form onSubmit={this.handleSubmit}>
           <label>Email
-            <input onChange={this.handleChange} type="email" />
+            <input onChange={this.handleChange} type="email" value={this.state.email} />
           </label>
           <input type="submit" value="Sign Up" />
         </form>
